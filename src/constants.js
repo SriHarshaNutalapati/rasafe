674edf470d7a3c53b72c2b7c9ebc544a96f94b08
= <vnutalap@umd.edu>
2024-05-05 01:40:03 -0400

    added backend

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,2 @@
-export const BACKEND_API_URL = "http://ec2-3-14-72-78.us-east-2.compute.amazonaws.com:8080"
+export const BACKEND_API_URL = "http://ec2-18-217-53-220.us-east-2.compute.amazonaws.com:8080"
 // export const BACKEND_API_URL = "http://localhost:8080"
